62104f435666afbc48baaf17fd4a86e86f27dbeb
Marcin Bartmiński <mbdjj@o2.pl>
2023-08-19 17:50:09 +0200

    Initial Commit

---
+++ TimeCalc/TimeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCalcApp.swift
+//  TimeCalc
+//
+//  Created by Marcin Bartminski on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
